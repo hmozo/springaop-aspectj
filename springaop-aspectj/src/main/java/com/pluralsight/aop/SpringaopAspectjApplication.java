92105244e79d80ec2f541194dddaa2722e3a22d7
Hugo Mozo <hmozo@outlook.com>
2020-11-04 12:16:46 +0100

    Config

---
+++ springaop-aspectj/src/main/java/com/pluralsight/aop/SpringaopAspectjApplication.java
@@ -0,0 +1,14 @@
+package com.pluralsight.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringaopAspectjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaopAspectjApplication.class, args);
+	}
+
+}
---
+++ springaop-aspectj/src/main/java/com/pluralsight/aop/configuration/SimpleAspectConfig.java
@@ -0,0 +1,12 @@
+package com.pluralsight.aop.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan(basePackages = "com.pluralsight.aop.simpleaspect")
+public class SimpleAspectConfig {
+
+}
